1db744f66e6535bb19e17e93e62050736ee77c10
MatthewPeoples <mattjoepeo@gmail.com>
2026-08-29 14:52:32 +0100

    Add CMake build with chess_lib and chess_uci targets

---
+++ apps/uci/main.cpp
@@ -0,0 +1,7 @@
+#include <chess/bitboard.hpp>
+
+#include <iostream>
+
+int main() {
+    std::cout << chess::square_index(4,3) << '\n';
+}
---
+++ include/chess/bitboard.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace chess {
+    int square_index(int file, int rank);
+}
